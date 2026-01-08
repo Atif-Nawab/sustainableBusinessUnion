c2c862a4dbe5ab0c0fa1645e4d6378d12a61e146
Atif-Nawab <atifnawab396@gmail.com>
2026-01-08 23:28:29 +0530

    adding env port

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const path = require("path");
 
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
+ const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
